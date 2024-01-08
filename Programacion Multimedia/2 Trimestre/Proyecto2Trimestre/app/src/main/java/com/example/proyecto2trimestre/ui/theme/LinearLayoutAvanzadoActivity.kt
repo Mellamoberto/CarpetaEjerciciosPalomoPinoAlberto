02d488537904e643a6ad18b6030c686c483f88d2
Mellamoberto <app-del-96@hotmail.com>
2024-01-08 11:20:47 +0100

    Ejercicios P.Multimedia Views y Constraint 08/01

---
+++ Programacion Multimedia/2 Trimestre/Proyecto2Trimestre/app/src/main/java/com/example/proyecto2trimestre/ui/theme/LinearLayoutAvanzadoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.proyecto2trimestre.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyecto2trimestre.R
+
+class LinearLayoutAvanzadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_avanzado)
+    }
+}
