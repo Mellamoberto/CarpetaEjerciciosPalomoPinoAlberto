f3ccfea8bb8892e6c27013258a7fc94d892a6d95
Mellamoberto <app-del-96@hotmail.com>
2024-01-09 14:20:26 +0100

    Clase P.Multimedia 09/01

---
+++ Programacion Multimedia/2 Trimestre/PhonelandProyectPalomoPinoAlberto/app/src/main/java/com/example/phonelandproyectpalomopinoalberto/FirstApp.kt
@@ -0,0 +1,11 @@
+package com.example.phonelandproyectpalomopinoalberto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
