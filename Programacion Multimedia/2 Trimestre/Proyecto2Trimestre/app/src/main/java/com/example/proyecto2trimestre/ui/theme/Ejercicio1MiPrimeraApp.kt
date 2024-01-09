d834b7fed08f3ca7013b6ba9574831ac425492d5
Mellamoberto <app-del-96@hotmail.com>
2024-01-09 13:34:16 +0100

    3 Ejercicios MiPrimeraApp 09/01

---
+++ Programacion Multimedia/2 Trimestre/Proyecto2Trimestre/app/src/main/java/com/example/proyecto2trimestre/ui/theme/Ejercicio1MiPrimeraApp.kt
@@ -0,0 +1,12 @@
+package com.example.proyecto2trimestre.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyecto2trimestre.R
+
+class Ejercicio1MiPrimeraApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio1_mi_primera_app)
+    }
+}
---
+++ Programacion Multimedia/2 Trimestre/Proyecto2Trimestre/app/src/main/java/com/example/proyecto2trimestre/ui/theme/Ejercicio2MiPrimeraApp.kt
@@ -0,0 +1,12 @@
+package com.example.proyecto2trimestre.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyecto2trimestre.R
+
+class Ejercicio2MiPrimeraApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio2_mi_primera_app)
+    }
+}
