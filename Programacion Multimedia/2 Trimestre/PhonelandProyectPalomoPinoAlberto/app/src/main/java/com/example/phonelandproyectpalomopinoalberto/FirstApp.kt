812da1a39239ea748ab8e2c155ecbcf6ab1f63d2
Mellamoberto <app-del-96@hotmail.com>
2024-01-12 14:26:19 +0100

    Clase P.Multimedia 12/01

--- Programacion Multimedia/2 Trimestre/PhonelandProyectPalomoPinoAlberto/app/src/main/java/com/example/phonelandproyectpalomopinoalberto/FirstApp.kt
+++ Programacion Multimedia/2 Trimestre/PhonelandProyectPalomoPinoAlberto/app/src/main/java/com/example/phonelandproyectpalomopinoalberto/FirstApp.kt
@@ -2,10 +2,26 @@ package com.example.phonelandproyectpalomopinoalberto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import android.content.Intent
+
 
 class FirstApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, Result::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+        }
     }
 }
--- Programacion Multimedia/2 Trimestre/PhonelandProyectPalomoPinoAlberto/app/src/main/java/com/example/phonelandproyectpalomopinoalberto/Result.kt
+++ Programacion Multimedia/2 Trimestre/PhonelandProyectPalomoPinoAlberto/app/src/main/java/com/example/phonelandproyectpalomopinoalberto/Result.kt
@@ -2,10 +2,15 @@ package com.example.phonelandproyectpalomopinoalberto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.util.*
 
 class Result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val tvResult=findViewById<TextView>(R.id.tvResult)
+        val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvResult.text = "Hola $name"
     }
 }
