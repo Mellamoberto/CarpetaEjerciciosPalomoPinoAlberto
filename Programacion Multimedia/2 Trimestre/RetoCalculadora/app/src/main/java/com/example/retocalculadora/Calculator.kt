08d54c56102d6486773929d398182701ea87a001
Mellamoberto <app-del-96@hotmail.com>
2024-01-18 22:17:19 +0100

    Update Calculator.kt

--- Programacion Multimedia/2 Trimestre/RetoCalculadora/app/src/main/java/com/example/retocalculadora/Calculator.kt
+++ Programacion Multimedia/2 Trimestre/RetoCalculadora/app/src/main/java/com/example/retocalculadora/Calculator.kt
@@ -2,6 +2,10 @@ package com.example.retocalculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 
 class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
