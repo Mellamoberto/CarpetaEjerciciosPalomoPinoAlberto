c689e3d263933b7521e6efa8e8395067468cca27
Mellamoberto <app-del-96@hotmail.com>
2024-01-22 19:26:53 +0100

    Gestion Archivos

---
+++ Programacion Multimedia/2 Trimestre/EjercicioCardView/app/src/main/java/com/example/ejercicio3/EjercicioCardview.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EjercicioCardview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio_cardview)
+    }
+}
