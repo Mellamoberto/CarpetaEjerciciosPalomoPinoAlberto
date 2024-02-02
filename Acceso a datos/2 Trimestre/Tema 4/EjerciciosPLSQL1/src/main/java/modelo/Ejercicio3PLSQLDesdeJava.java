3736118f8efe414cf39c6155031c3513bf2c570d
Mellamoberto <app-del-96@hotmail.com>
2024-02-02 12:49:43 +0100

    Ejercicios RecicledView 02/02/2024

---
+++ Acceso a datos/2 Trimestre/Tema 4/EjerciciosPLSQL1/src/main/java/modelo/Ejercicio3PLSQLDesdeJava.java
@@ -0,0 +1,43 @@
+package modelo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+
+public class Ejercicio3PLSQLDesdeJava {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+
+        try (Session session = sessionFactory.openSession()) {
+            session.doWork(connection -> {
+                String procedimientoAlmacenado = "{ CALL procedimiento_Alumno(?) }";
+
+
+                try (CallableStatement statement = connection.prepareCall(procedimientoAlmacenado)) {
+
+                    statement.registerOutParameter(1, Types.VARCHAR);
+
+                    statement.execute();
+
+                    String output = statement.getString(1);
+
+                    System.out.println(output);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
